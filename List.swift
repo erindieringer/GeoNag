d036a775b752c4c42b4477f62e23539d492c91bf
Katie Williams <kaw1@andrew.cmu.edu>
2016-11-18 13:47:51 -0500

    Added basic List View Models

---
+++ List.swift
@@ -0,0 +1,18 @@
+//
+//  List.swift
+//  iOSApp
+//
+//  Created by Katie Williams on 11/18/16.
+//  Copyright © 2016 Katie Williams. All rights reserved.
+//
+import UIKit
+
+struct List {
+    let id: Int
+    let name: String
+    let date_created: NSDate
+    let date_modified: NSDate
+    let shared: Bool
+    let user: Int
+    
+}
---
+++ ListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ListViewModel.swift
+//  iOSApp
+//
+//  Created by Katie Williams on 11/18/16.
+//  Copyright © 2016 Katie Williams. All rights reserved.
+//
+
+import Foundation
+
+class ListViewModel {
+    
+}
