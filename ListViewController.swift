fec5a75e2687ee90145d5635fc70b688e19744e8
Erin Dieringer <ediering@andrew.cmu.edu>
2016-11-18 14:51:28 -0500

    populated ListViewController

--- ListViewController.swift
+++ ListViewController.swift
@@ -25,5 +25,30 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let selectedRow = tableView.indexPathForSelectedRow {
+            tableView.deselectRowAtIndexPath(selectedRow, animated: true)
+        }
+    }
+    
+    //Table View
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int)  -> Int {
+        return viewModel.numberOfRows()
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        cell.textLabel?.text =  viewModel.titleForRowAtIndexPath(indexPath)
+        return cell
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let detailVC = segue.destinationViewController as? ListDetailViewController,
+            cell = sender as? UITableViewCell,
+            indexPath = tableView.indexPathForCell(cell) {
+            detailVC.viewModel =  viewModel.detailViewModelForRowAtIndexPath(indexPath)
+        }
+    }
     
 }
