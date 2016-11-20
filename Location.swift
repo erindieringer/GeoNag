f0a95818c9ca244e731aedd22eb6fcde3d4ac7f3
Erin Dieringer <ediering@andrew.cmu.edu>
2016-11-19 20:42:59 -0500

    added location struct for location model

--- Location.swift
+++ Location.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2016 Katie Williams. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreLocation
+import MapKit
+
+struct Location {
+    let id: Int
+    let latitude: CLLocationDegrees
+    let longitude: CLLocationDegrees
+    let name: String
+    let phone: String
+    let address: String
+    let placemark: MKPlacemark
+    
+}
