4148cfa73fb17c921071cbb13de295859fd2d237
Erin Dieringer <ediering@andrew.cmu.edu>
2016-11-19 20:58:41 -0500

    currentlocation class added to keep track of current location and the matching map items

---
+++ CurrentLocation.swift
@@ -0,0 +1,58 @@
+//
+//  LocationManager.swift
+//  iOSApp
+//
+//  Created by Erin Dieringer on 11/19/16.
+//  Copyright © 2016 Katie Williams. All rights reserved.
+//
+
+import MapKit
+import CoreLocation
+
+class CurrentLocation {
+    
+    let manager = CLLocationManager()
+    var matchingItems:[MKMapItem] = []
+    let latitude: CLLocationDegrees
+    let longitude: CLLocationDegrees
+    
+    func getCurrentLocation() {
+        manager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            manager.distanceFilter = kCLDistanceFilterNone
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.startUpdatingLocation()
+        }
+        
+        if let currLocation = manager.location {
+            self.latitude = currLocation.coordinate.latitude
+            self.longitude = currLocation.coordinate.longitude
+        }
+    }
+    
+    func findMatchingItems(tag: String, region: MKCoordinateRegion) -> [MKMapItem] {
+        // region will have to be passed in (template below)
+        //      let span = MKCoordinateSpanMake(0.05, 0.05)
+        //      let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = tag
+        request.region = region
+        
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+            for item in self.matchingItems {
+                print(item)
+            
+            }
+        }
+        
+        return matchingItems
+    }
+    
+ 
+}
