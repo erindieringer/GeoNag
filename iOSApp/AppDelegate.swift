dd329b7473bfe10c5fd1e399211b0d82fb6c820f
Erin Dieringer <ediering@andrew.cmu.edu>
2016-12-06 12:54:33 -0500

    added authotization for locatoin background in info.plist

--- iOSApp/AppDelegate.swift
+++ iOSApp/AppDelegate.swift
@@ -114,6 +114,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate , CLLocationManagerDelegat
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         isExecutingInBackground = true
+        locationManager.delegate = self
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startMonitoringSignificantLocationChanges()
     }
     
     func applicationWillEnterForeground(application: UIApplication) {
