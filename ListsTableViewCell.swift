663338c11ae74f906643e9b5bc57d818e96bbad1
Katie Williams <kaw1@andrew.cmu.edu>
2016-12-08 17:09:15 -0500

    updated files

--- ListsTableViewCell.swift
+++ ListsTableViewCell.swift
@@ -1,8 +1,8 @@
 //
-//  ListTableViewCell.swift
+//  ListsTableViewCell.swift
 //  iOSApp
 //
-//  Created by Katie Williams on 12/8/16.
+//  Created by Katie Williams on 11/28/16.
 //  Copyright © 2016 Katie Williams. All rights reserved.
 //
 
