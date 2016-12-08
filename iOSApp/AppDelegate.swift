87c20c61da2592b0fe1ee18e61d329036a73b04a
Erin Dieringer <ediering@andrew.cmu.edu>
2016-12-07 21:03:54 -0500

    deleted getTags size

--- iOSApp/AppDelegate.swift
+++ iOSApp/AppDelegate.swift
@@ -82,17 +82,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate , CLLocationManagerDelegat
         let notificationSettings = UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert, categories: nil)
         UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
         
-        getListTags()
         return true
         
     }
     
-//    func getTagsSize() {
-//        let tags = tagView.fetchAllTags()
-//        for tag in tags! {
-//            print(tag.valueForKey("lists")?count)
-//        }
-//    }
     
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
